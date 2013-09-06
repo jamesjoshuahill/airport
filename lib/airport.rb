ca865db858b33a56406acb811c47e2951fde0f38
Josh Hill <jamesjoshuahill@gmail.com>
2013-09-06 15:11:00 +0100

    Made internal methods private in airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -10,32 +10,6 @@ class Airport
     put_new_planes_in_hanger(no_of_planes)
   end
 
-  def check_arguments(no_of_planes, capacity)
-    raise ArgumentError.new(
-      'Cannot have more planes than there is capacity for.'
-    ) if no_of_planes > capacity
-  end
-
-  def put_new_planes_in_hanger(no_of_planes)
-    no_of_planes.times { @hanger << new_plane }
-  end
-
-  def new_plane
-    Plane.new(self)
-  end
-
-  def sunny_weather?
-    @weather.state == :sunny
-  end
-
-  def has_space_in_hanger?
-    hanger.count < capacity
-  end
-
-  def has_sunny_weather_and_no_bomb_scare?
-    sunny_weather? and !has_a_bomb_scare?
-  end
-
   def clear_to_land?
     has_space_in_hanger? and has_sunny_weather_and_no_bomb_scare?
   end
@@ -62,5 +36,33 @@ class Airport
 
   def call_off_bomb_scare
     @bomb_scare = false
+  end
+
+  private
+  
+  def check_arguments(no_of_planes, capacity)
+    raise ArgumentError.new(
+      'Cannot have more planes than there is capacity for.'
+    ) if no_of_planes > capacity
+  end
+
+  def put_new_planes_in_hanger(no_of_planes)
+    no_of_planes.times { @hanger << new_plane }
+  end
+
+  def new_plane
+    Plane.new(self)
+  end
+
+  def sunny_weather?
+    @weather.state == :sunny
+  end
+
+  def has_space_in_hanger?
+    hanger.count < capacity
+  end
+
+  def has_sunny_weather_and_no_bomb_scare?
+    sunny_weather? and !has_a_bomb_scare?
   end
 end
